b165c5658a411da3c1cb7ef49bb912a1d8fa2880
Maheus Ferrante Silva <matheusferrantesilvafrs@gmail.com>
2026-07-06 21:10:08 -0300

    Update AppDbContextFactory.cs

--- Data/AppDbContextFactory.cs
+++ Data/AppDbContextFactory.cs
@@ -7,7 +7,7 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
 
-        var connectionString = "server=localhost;user=root;password=...;database=jobtracker;port=3306;";
+        var connectionString = "server=localhost;user=root;password=...;database=jobtracker;port=;";
 
         optionsBuilder.UseMySql
         (
